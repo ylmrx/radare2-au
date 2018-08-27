cd65123c265dbd1a64bcfa7d435e870d3282693f
pancake <pancake@nopcode.org>
2018-08-27 23:54:12 +0200

    Update asm_au for r2-2.9

--- asm_au.c
+++ asm_au.c
@@ -4,52 +4,53 @@
 #include <r_lib.h>
 #include "cpu.h"
 
-static int assemble(RAsm *a, RAsmOp *op, const char *buf) {
-	char *arg = strdup (buf);
+static int assemble(RAsm *a, RAsmOp *op, const char *str) {
+	char *arg = strdup (str);
 	r_str_replace_char (arg, ',', ' ');
 	RList *args = r_str_split_list (arg, " ");
 	const char *mnemonic = r_list_get_n (args, 0);
+	ut8 *buf = r_asm_op_get_buf (op);
 	op->size = -1;
 	if (!strcmp (mnemonic, "nop")) {
-		op->buf[0] = AUCPU_OP_NOP;
+		buf[0] = AUCPU_OP_NOP;
 		op->size = 4;
 	} else if (!strcmp (mnemonic, "mov")) {
 		const char *arg0 = r_list_get_n (args, 1);
-		op->buf[0] = AUCPU_OP_MOV;
+		buf[0] = AUCPU_OP_MOV;
 		op->size = 4;
 		if (arg0 && *arg0 == 'r') {
-			op->buf[1] = atoi (arg0 + 1);
+			buf[1] = atoi (arg0 + 1);
 			const char *arg1 = r_list_get_n (args, 2);
 			if (arg1) {
 				if (*arg1 == 'r') {
-					op->buf[0] = AUCPU_OP_MOVREG;
-					op->buf[1] |= atoi (arg0 + 1);
+					buf[0] = AUCPU_OP_MOVREG;
+					buf[1] |= atoi (arg0 + 1);
 					op->size = 2;
 				} else {
 					ut16 v = r_num_math (NULL, arg1);
-					op->buf[2] = (v >> 8) & 0xff;
-					op->buf[3] = (v & 0xff);
+					buf[2] = (v >> 8) & 0xff;
+					buf[3] = (v & 0xff);
 				}
 			}
 		}
 	} else if (!strcmp (mnemonic, "trap")) {
 		op->size = 2;
-		op->buf[0] = AUCPU_OP_TRAP;
+		buf[0] = AUCPU_OP_TRAP;
 	} else if (!strcmp (mnemonic, "wave")) {
 		op->size = 4;
 		// RETHINK OP, r0, r1 must be 2nd arg
 		// wsin r0, r1, r2
 	} else if (!strcmp (mnemonic, "play")) {
-		op->buf[0] = AUCPU_OP_PLAY;
+		buf[0] = AUCPU_OP_PLAY;
 		op->size = 2;
 		const char *arg0 = r_list_get_n (args, 1);
 		if (arg0 && *arg0 == 'r') {
 			const char *arg1 = r_list_get_n (args, 2);
 			if (arg1 && *arg1 == 'r') {
 				ut16 v = r_num_math (NULL, arg1);
-				op->buf[0] = AUCPU_OP_PLAYREG;
-				op->buf[1] = atoi (arg0 + 1);
-				op->buf[1] |= atoi (arg1 + 1) << 4;
+				buf[0] = AUCPU_OP_PLAYREG;
+				buf[1] = atoi (arg0 + 1);
+				buf[1] |= atoi (arg1 + 1) << 4;
 				op->size = 2;
 			}
 		}
@@ -74,7 +75,7 @@ static const char *waveType(const ut8 t) {
 
 static void invalid (RAsmOp *op, const ut8 *buf) {
 	st16 *dword = (st16*)buf;
-	snprintf (op->buf_asm, sizeof (op->buf_asm), ".short %d", *dword);
+	r_asm_op_set_asm (op, sdb_fmt (".short %d", *dword));
 	op->size = 2;
 }
 
@@ -85,20 +86,20 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	op->size = 4;
 	switch (buf[0]) {
 	case AUCPU_OP_NOP:
-		strcpy (op->buf_asm, "nop");
+		r_asm_op_set_asm (op, "nop");
 		break;
 	case AUCPU_OP_MOVREG:
 		{
 			int r0 = buf[1] & 0xf;
 			int r1 = (buf[1] & 0xf0);
-			snprintf (op->buf_asm, sizeof (op->buf_asm), "mov r%d, r%d", r0, r1);
+			r_asm_op_set_asm (op, sdb_fmt ("mov r%d, r%d", r0, r1));
 		}
 		break;
 	case AUCPU_OP_MOV:
 		{
 			int r = buf[1];
 			int v = (buf[2] << 8) | buf[3];
-			snprintf (op->buf_asm, sizeof (op->buf_asm), "mov r%d, %d", r, v);
+			r_asm_op_set_asm (op, sdb_fmt ("mov r%d, %d", r, v));
 		}
 		break;
 	case AUCPU_OP_WAVE:
@@ -107,7 +108,7 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 			int freq = ((buf[2]<< 8) | buf[3]) << 2;
 			const char *type = waveType(buf[1]);
 			if (type) {
-				snprintf (op->buf_asm, sizeof (op->buf_asm), "wave %s, %d", type, freq);
+				r_asm_op_set_asm (op, sdb_fmt ("wave %s, %d", type, freq));
 			} else {
 				invalid (op, buf);
 			}
@@ -119,10 +120,10 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 			int r1 = (buf[1] & 0xf0);
 			if (r1) {
 				int v = (buf[2] << 8) | buf[3];
-				snprintf (op->buf_asm, sizeof (op->buf_asm), "wait %d", v);
+				r_asm_op_set_asm (op, sdb_fmt ("wait %d", v));
 				op->size = 4;
 			} else {
-				snprintf (op->buf_asm, sizeof (op->buf_asm), "wait r%d", r0);
+				r_asm_op_set_asm (op, sdb_fmt ("wait r%d", r0));
 				op->size = 2;
 			}
 		}
@@ -137,27 +138,27 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 					v = -v;
 				}
 				ut64 addr = a->pc + v + 4;
-				snprintf (op->buf_asm, sizeof (op->buf_asm), "jmp 0x%08"PFMT64x, addr);
+				r_asm_op_set_asm (op, sdb_fmt ("jmp 0x%08"PFMT64x, addr));
 				op->size = 4;
 			} else {
-				snprintf (op->buf_asm, sizeof (op->buf_asm), "jmp r%d", r0);
+				r_asm_op_set_asm (op, sdb_fmt ("jmp r%d", r0));
 				op->size = 2;
 			}
 		}
 		break;
 	case AUCPU_OP_TRAP:
-		strcpy (op->buf_asm, "trap");
+		r_asm_op_set_asm (op, "trap");
 		op->size = 2;
 		break;
 	case AUCPU_OP_PLAY: // DEPRECATE?
-		strcpy (op->buf_asm, "play");
+		r_asm_op_set_asm (op, "play");
 		op->size = 2;
 		break;
 	case AUCPU_OP_PLAYREG:
 		{
 			int r0 = buf[1] & 0xf;
 			int r1 = (buf[1] & 0xf0) >> 4;
-			snprintf (op->buf_asm, sizeof (op->buf_asm), "play r%d, r%d", r0, r1);
+			r_asm_op_set_asm (op, sdb_fmt ("play r%d, r%d", r0, r1));
 		}
 		break;
 	default:
